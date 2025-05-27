48790769a93cb42347c37c399abf2ee510ebd8e3
Msrevalli <81502907+Msrevalli@users.noreply.github.com>
2025-05-27 09:58:51 +0530

    commit

--- app6/src/books/routes.py
+++ app6/src/books/routes.py
@@ -4,12 +4,12 @@ from src.books.schemas import Book, BookUpdateModel
 from typing import List
 book_router = APIRouter()
 
-@book_router.get("/books", response_model=List[Book])
+@book_router.get("/", response_model=List[Book])
 async def get_all_books():
     return books
 
 
-@book_router.post("/books", status_code=status.HTTP_201_CREATED)
+@book_router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_a_book(book_data: Book) -> dict:
     new_book = book_data.model_dump()
 
@@ -18,7 +18,7 @@ async def create_a_book(book_data: Book) -> dict:
     return new_book
 
 
-@book_router.get("/book/{book_id}")
+@book_router.get("/{book_id}")
 async def get_book(book_id: int) -> dict:
     for book in books:
         if book["id"] == book_id:
@@ -27,7 +27,7 @@ async def get_book(book_id: int) -> dict:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
 
-@book_router.patch("/book/{book_id}")
+@book_router.patch("/{book_id}")
 async def update_book(book_id: int,book_update_data:BookUpdateModel) -> dict:
 
     for book in books:
@@ -42,7 +42,7 @@ async def update_book(book_id: int,book_update_data:BookUpdateModel) -> dict:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
 
-@book_router.delete("/book/{book_id}",status_code=status.HTTP_204_NO_CONTENT)
+@book_router.delete("/{book_id}",status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int):
     for book in books:
         if book["id"] == book_id:
